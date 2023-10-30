ec66fb32d22ee67b95de0d8b97256e131e8e4488
CaioFD <caiodiniz200204@gmail.com>
2023-10-30 09:48:44 -0300

    ..

--- LISTAS/Lista 6/E2.c
+++ LISTAS/Lista 6/E2.c
@@ -1,145 +1,5 @@
-#include <stdio.h> 
-
-typedef struct
-{
-    int ano;
-    int mes;
-    int dia;
-} Calendario;
-
-
-Calendario verificacao(int dia, int mes, int ano)
-{
-    Calendario data;
-
-    if (ano >= 0 && mes >= 1 && mes <= 12)
-    {
-        switch (mes)
-        {
-        case 1:
-
-            break;
-        case 2:
-            if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))
-            {
-                if (dia >= 1 && dia <= 29)
-                {
-                    data.ano = ano;
-                    data.mes = mes;
-                    data.dia = dia;
-                }
-                else
-                {
-                    data.ano = 0;
-                    data.mes = 0;
-                    data.dia = 0;
-                }
-            }
-            else
-            {
-                if (dia >= 1 && dia <= 28)
-                {
-                    data.ano = ano;
-                    data.mes = mes;
-                    data.dia = dia;
-                }
-                else
-                {
-                    data.ano = 0;
-                    data.mes = 0;
-                    data.dia = 0;
-                }
-            }
-
-            break;
-        case 4: 
-
-            if (dia >= 1 && dia <= 31)
-            {
-                data.ano = ano;
-                data.mes = mes;
-                data.dia = dia;
-            }
-            else
-            {
-                data.ano = 0;
-                data.mes = 0;
-                data.dia = 0;
-            }
-
-            break;
-        case 6: 
-
-            if (dia >= 1 && dia <= 31)
-            {
-                data.ano = ano;
-                data.mes = mes;
-                data.dia = dia;
-            }
-            else
-            {
-                data.ano = 0;
-                data.mes = 0;
-                data.dia = 0;
-            }
-
-            break;
-        case 9: 
-
-            if (dia >= 1 && dia <= 31)
-            {
-                data.ano = ano;
-                data.mes = mes;
-                data.dia = dia;
-            }
-            else
-            {
-                data.ano = 0;
-                data.mes = 0;
-                data.dia = 0;
-            }
-
-            break;
-        case 11: 
-
-            if (dia >= 1 && dia <= 31)
-            {
-                data.ano = ano;
-                data.mes = mes;
-                data.dia = dia;
-            }
-            else
-            {
-                data.ano = 0;
-                data.mes = 0;
-                data.dia = 0;
-            }
-
-            break;
-        default:
-            if (dia >= 1 && dia <= 31)
-            {
-                data.ano = ano;
-                data.mes = mes;
-                data.dia = dia;
-            }
-            else
-            {
-                data.ano = 0;
-                data.mes = 0;
-                data.dia = 0;
-            }
-        }
-    }
-    else
-    {
-        data.ano = 0;
-        data.mes = 0;
-        data.dia = 0;
-    }
-
-    return data;
-}
+#include <stdio.h>
+#include "E1-4.h"
 
 int main()
 {
@@ -150,15 +10,15 @@ int main()
 
     printf("digite um dia do ano\n");
     scanf("%d", &dia);
-   
+    getchar();
     printf("digite um mes do ano\n");
     scanf("%d", &mes);
-    
+    getchar();
     printf("digite um ano\n");
     scanf("%d", &ano);
-   
+    getchar();
 
-    Calendario data = verificacao(dia, mes, ano);
+    Data data = verificacao(dia, mes, ano);
 
     if (data.ano == 0 && data.mes == 0 && data.dia == 0) {
         printf("Data invalida!\n");
@@ -166,5 +26,6 @@ int main()
         printf("Data valida: %d/%d/%d\n", data.dia, data.mes, data.ano);
     }
 
-return 0;
+
+    return 0;
 }
